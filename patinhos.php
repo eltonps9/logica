4eec7f001d65032bbe54993435976ba768f31bb4
Elton Passos <81651224+eltonps9@users.noreply.github.com>
2024-11-05 22:51:47 -0300

    Update patinhos.php

--- patinhos.php
+++ patinhos.php
@@ -1,3 +1,9 @@
+
+<p>total de patos: </p> 
+<form action="" method="get">
+    <input type="number" name="number">
+    <input type="submit" value="tocar">
+</form>
 <?php
 
 /*
@@ -13,42 +19,55 @@ Na beira do mar
 A mamãe gritou: Quá, quá, quá, quá
 E os cinco patinhos voltaram de lá
 */
-$patos = readline('total de patos aqui:');
 
-if($patos<=1 || $patos>10){
-    echo "numero de patos nao aceito.\n";
-    exit;
-}
+//$patos = readline('total de patos aqui:');
 
-for($i=$patos;$i>0;$i--){
+$patos=$_GET['number'];
 
-    echo "\n";
-    if($i==1){
-        echo $i." patinho foi passear\n";
-    }else{
-        echo $i." patinhos foram passear\n";
+if(isset($patos)){
+    if($patos<=1 || $patos>10){
+        echo "numero de patos nao aceito. <br>";
+        exit;
     }
 
-    echo "Alem das montanhas\n";
-    echo "Para brincar\n";
-    echo "A mamae gritou: Qua, qua, qua, qua\n";
+    for($i=$patos;$i>0;$i--){
 
-    switch($i){
-        case '2':
-            echo "Mas so 1 patinho voltou de la\n";
-            break;
-        case '1':
-            echo "Nenhum patinho voltou de la\n";
-            break;
-        default:
-            echo "Mas so ".($i-1)." patinhos voltaram de la\n";
-            break;
-    }    
+        echo "\n";
+        if($i==1){
+            echo $i." patinho foi passear ";
+            echo "<img src='./image/pato-caminando.gif' style='width:30px'><br>";
+        }else{
+            echo $i." patinhos foram passear";
+
+            for($p=0;$p<$i;$p++){
+                echo "<img src='./image/pato-caminando.gif' style='width:30px'>";
+            }
+        }
+        echo "<br>";
+        echo "Alem das montanhas <br>";
+        echo "Para brincar <br><br>";
+        echo "A mamae gritou: Qua, qua, qua, qua <br><br>";
+
+        switch($i){
+            case '2':
+                echo "Mas so 1 patinho voltou de la <br><hr>";
+                break;
+            case '1':
+                echo "Nenhum patinho voltou de la <br><hr>";
+                break;
+            default:
+                echo "Mas so ".($i-1)." patinhos voltaram de la <br><hr>";
+                break;
+        }    
+    }
+    echo "<img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2JrM3plcXJ3ZWZrYTM5eWp2a2NmZTdkYnpkNWhoNnRycmppcnZmZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l4KhKRcaYb43LVqq4/giphy.webp' style='width:60px'>";
+    echo " <br>";
+    echo "A mamae patinha foi procurar <br>";
+    echo "Alem das montanhas <br>";
+    echo "Na beira do mar <br>";
+    echo "A mamae gritou: Qua, qua, qua, qua <br>";
+    echo "E os ".$patos." patinhos voltaram de la <br>";
+    for($p=0;$p<$patos;$p++){
+        echo "<img src='./image/pato-caminando.gif' style='width:30px'>";
+    }
 }
-
-echo "\n";
-echo "A mamae patinha foi procurar\n";
-echo "Alem das montanhas\n";
-echo "Na beira do mar\n";
-echo "A mamae gritou: Qua, qua, qua, qua\n";
-echo "E os ".$patos." patinhos voltaram de la\n";
